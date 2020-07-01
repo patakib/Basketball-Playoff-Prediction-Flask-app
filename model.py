7c1216f35f8e713a740109b4c57c8fa69b103e80
Balint Pataki <patakibalintka@gmail.com>
2020-07-01 13:05:07 +0200

    Update model.py

--- model.py
+++ model.py
@@ -41,7 +41,7 @@ teamlabels = pd.DataFrame(
      'Playoff': labels
          })
         
-datacolumns = ["M1","M2","M3","M4","M5","M6","M7","M8","M9","M10"]
+datacolumns = ["M1","M2","M3","M4","M5","M6","M7","M8","M9","M10","pts","opp_pts"]
         # ,"M11","M12","M13","M14","M15","M16","M17","M18","M19","M20"
 for column in datacolumns:
     teamlabels[column] = np.nan
@@ -51,10 +51,13 @@ teamlabels = teamlabels.set_index('TeamYear')
         
 for team in allteams:
     teamrecord = df[(df['TeamYear']==team) & (df['is_playoffs']==0)]
-    teamrecord = teamrecord[['game_result']]
+    teamrecord = teamrecord[['game_result','pts','opp_pts']]
     teamrecord = teamrecord.head(10)
-    teamrecord = teamrecord.rename(columns={'game_result': team})
+    teamrecord.at['pts','game_result'] = teamrecord['pts'].sum()
+    teamrecord.at['opp_pts','game_result'] = teamrecord['opp_pts'].sum()
+    teamrecord = teamrecord[['game_result']]
     teamrecord = teamrecord.T
+    teamrecord = teamrecord.rename(index={'game_result': team})
     teamrecord.columns = datacolumns
     teamlabels.update(teamrecord)
     lst = [teamrecord]
@@ -63,13 +66,19 @@ for team in allteams:
 y = teamlabels[['Playoff']]
 teamlabels = teamlabels.drop(['Playoff'], axis=1)
 X = teamlabels.copy()
+X_pts = X[['pts','opp_pts']]
+X = X.drop(['pts','opp_pts'],axis=1)
 X = pd.get_dummies(X)
 X['Win']=X['M1_W']+X['M2_W']+X['M3_W']+X['M4_W']+X['M5_W']+X['M6_W']+X['M7_W']+X['M8_W']+X['M9_W']+X['M10_W']
 X['Lost']=X['M1_L']+X['M2_L']+X['M3_L']+X['M4_L']+X['M5_L']+X['M6_L']+X['M7_L']+X['M8_L']+X['M9_L']+X['M10_L']
-X=X[['Win','Lost']]
+X = X[['Win','Lost']]
+X['pts'] = X_pts['pts']
+X['opp_pts'] = X_pts['opp_pts']
 
+#-------------
 #split dataset into train and test set
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+#------------
 
 # log_clf = LogisticRegression(solver="lbfgs", random_state=42)
 # log_clf.fit(X_train, y_train.values.ravel())
@@ -95,9 +104,12 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_
 # print("Recall: {:.2f}%".format(100 * recall_score(y_test.values.ravel(),y_pred)))
 # print("F1: {:.2f}%".format(100 * f1_score(y_test.values.ravel(),y_pred)))
 
-#fit the dataset into Random Forest Classifier algorithm
+#-----------
+# fit the dataset into Random Forest Classifier algorithm
 rnd_clf = RandomForestClassifier(n_estimators=500, max_leaf_nodes=16, n_jobs=-1)
 rnd_clf.fit(X_train, y_train.values.ravel())
+#-----------------
+
 # y_pred = rnd_clf.predict(X_test)
 # print(confusion_matrix(y_test.values.ravel(),y_pred))
 # print("Precision: {:.2f}%".format(100 * precision_score(y_test.values.ravel(),y_pred)))
@@ -107,11 +119,15 @@ rnd_clf.fit(X_train, y_train.values.ravel())
 #checkpoint for the predictor
 # print(rnd_clf.predict([[4,6]]))
 
+
+
+
+
+#--------------
 # Saving model to disk
 pickle.dump(rnd_clf, open('model.pkl','wb'))
 
 # Loading model to compare the results
 model = pickle.load(open('model.pkl','rb'))
-print(model.predict([[8, 2]]))
-
-
+print(model.predict([[4, 6, 100, 1100]]))
+#-----------------
